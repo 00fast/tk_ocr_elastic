5eec7e5005543a4e88041fccdcd698d091c9c9e6
00fast <128151569+00fast@users.noreply.github.com>
2023-07-11 17:53:10 +0900

    pdf file page and text extraction time check
    pdf file page and text extraction time check

--- Project/func/tk_ocr.py
+++ Project/func/tk_ocr.py
@@ -5,42 +5,61 @@ import numpy as np
 import magic
 from pdf2image import convert_from_path
 import mimetypes
+import time
 
 
 class TextExtractor:
     def __init__(self, file_path):
         self.file_path = file_path
         self.tk_contents = None
+        self.reader = easyocr.Reader(['ko', 'en'])
 
-    def process_file(self, file_path):
+    def process_file(self):
         mime = magic.Magic(mime=True)
         file_type = mime.from_file(self.file_path)
 
+        # 파일 type 별 text 추출 및 걸리는 시간 체크
         if file_type == "application/pdf" or mimetypes.guess_type(self.file_path)[0] == "application/pdf":
-            self.extract_text_from_pdf(file_path)
+            start_time = time.time()
+            self.extract_text_from_pdf(self.file_path)
+            end_time = time.time() - start_time
         elif file_type == "image/jpeg" or file_type == "image/png" :
-            self.extract_text_from_image(file_path)
+            start_time = time.time()
+            self.extract_text_from_image(self.file_path)
+            end_time = time.time() - start_time
         else:
-            print("옳바른 파일형식이 아닙니다..")
-        return self.tk_contents
+            print("올바른 파일형식이 아닙니다..")
 
+        num_pages = self.get_number_of_pages()
+
+        print(f"PDF 페이지 수: {num_pages}")
+        print(f"Text 추출 걸린 시간: {end_time} seconds")
+
+        return self.tk_contents
+        
+    # PDF 파일 페이지 확인
+    def get_number_of_pages(self):
+        raw_pdf = parser.from_file(self.file_path, xmlContent=True)
+        xml_content = raw_pdf['content']
+        num_pages = xml_content.count('<div class="page">')
+
+        return num_pages
+    
     def extract_text_from_pdf(self, file_path):
         # PDF 파일 TEXT 추출
         raw_pdf = parser.from_file(self.file_path)
         tk_contents = raw_pdf['content']
-        self.tk_contents = tk_contents.strip()
-        return self.tk_contents
+        if tk_contents is not None:
+            self.tk_contents = tk_contents.strip()
+        else :     
        
-
-        if self.tk_contents is None:
             print("이미지 PDF파일입니다.")
             
-            reader = easyocr.Reader(['ko'])
-
+            
             # PDF파일을 IMAGE로 변경
             images = convert_from_path(self.file_path)
 
-            self.tk_contents = []  # Use multiple pages
+            self.tk_contents = []  # 여러 페이지
 
             for page_num, img in enumerate(images):
                 # OpenCV 형태로 변환
@@ -48,30 +67,26 @@ class TextExtractor:
                 # gray가 평균적으로 더 잘 추출함
                 img_gray = cv2.cvtColor(img_cv, cv2.COLOR_BGR2GRAY)
                 # EasyOcr 사용
-                ocr_contents = reader.readtext(img_gray)
+                ocr_contents = self.reader.readtext(img_gray)
                 # EasyOcr 여러 결과값 중 텍스트만 저장
                 page_text = [result[1] for result in ocr_contents]
                 self.tk_contents.extend(page_text)
-            # tk_contents 문자형으로 변경
             self.tk_contents = '\n'.join(self.tk_contents)
-            self.tk_contents = self.tk_contents.replace("\\", "").replace("\'", "")
-            return self.tk_contents
+        
+        return self.tk_contents
 
     def extract_text_from_image(self, file_path):
         # EasyOCR Reader
-        reader = easyocr.Reader(['ko'])
-
+        
         img = cv2.imread(self.file_path)
         # gray가 평균적으로 더 잘 추출함
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # EasyOCR 사용
-        ocr_contents = reader.readtext(img_gray)
+        ocr_contents = self.reader.readtext(img_gray)
         # EasyOcr 여러 결과값 중 텍스트만 저장
         self.tk_contents = [result[1] for result in ocr_contents]
         self.tk_contents = '\n'.join(self.tk_contents)
         return self.tk_contents
-
-
 
 
 
